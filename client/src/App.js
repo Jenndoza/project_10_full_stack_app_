8a55eb7ed82a5ec53a7aa1f0d4c4190d354f208e
Jenndoza <j.mendo1997@gmail.com>
2022-08-25 01:33:37 -0500

    added components to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import {
   Switch
 } from 'react-router-dom';
 
+import CourseDetail from './components/CourseDetail';
+import Courses from './components/Courses';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components UpdateCourse';
 import Header from './components/Header';
 import Public from './components/Public';
 import NotFound from './components/NotFound';
@@ -13,14 +17,14 @@ import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
 import Authenticated from './components/Authenticated';
 
-import withContext from './Context';
+import useContext from './Context';
 import PrivateRoute from './PrivateRoute';
 
-const HeaderWithContext = withContext(Header);
-const AuthWithContext = withContext(Authenticated);
-const UserSignUpWithContext = withContext(UserSignUp);
-const UserSignInWithContext = withContext(UserSignIn);
-const UserSignOutWithContext = withContext(UserSignOut);
+const HeaderWithContext = useContext(Header);
+const AuthWithContext = useContext(Authenticated);
+const UserSignUpWithContext = useContext(UserSignUp);
+const UserSignInWithContext = useContext(UserSignIn);
+const UserSignOutWithContext = useContext(UserSignOut);
 
 export default () => (
   <Router>
